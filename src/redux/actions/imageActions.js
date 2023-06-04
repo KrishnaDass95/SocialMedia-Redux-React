ed43c5ed2e55dbf2f9ebdfe9357a7443f909cbdd
KrishnaDass95 <krishdass95@gmail.com>
2023-06-04 13:03:20 +0530

    Add a reducer file

--- src/redux/actions/imageActions.js
+++ src/redux/actions/imageActions.js
@@ -5,14 +5,12 @@ import { FETCH_POST_FROM_API, FETCH_POST_SUCCESS, FETCH_POST_FAILURE } from "./a
 // Action creator file
 export const fetchPostFromApi = () => {
     return{
-        loading: true,
         type : FETCH_POST_FROM_API
     }
 }
 
 export const fetchPostSuccess= (data) => {
     return{
-        loading: false,
         type: FETCH_POST_SUCCESS,
         payload: data
     }
@@ -20,7 +18,6 @@ export const fetchPostSuccess= (data) => {
 
 export const fetchPostFailure = (error) => {
     return {
-        loading: false, 
         type: FETCH_POST_FAILURE,
         error: error
     }
