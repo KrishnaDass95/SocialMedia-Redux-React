2b9b0a0754f60430a6357abc20f26c9a1e2bc86e
KrishnaDass95 <krishdass95@gmail.com>
2023-06-04 13:06:38 +0530

    install thunk, redux and make store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,8 @@
-import 
+import { applyMiddleware, createStore } from "redux";
+import { postReducer } from "./reducers/postReducer";
+import thunk from "redux-thunk";
+
+
+const store = createStore(postReducer, applyMiddleware(thunk));
+
+export default store;
